b8a914f129b6228fc0ad185d5be7d470ab170857
Ross Hathaway <hathaway.ross@gmail.com>
2019-03-29 15:49:12 -0400

    have basic files for store and reducers

---
+++ learn-redux/client/reducers/comments.js
@@ -0,0 +1,6 @@
+function comments(state = [], action) {
+  console.log('action from comments.js reducer:', action)
+  return state
+}
+
+export default comments
---
+++ learn-redux/client/reducers/posts.js
@@ -0,0 +1,6 @@
+function posts(state = [], action) {
+  console.log('action from posts.js reducer:', action)
+  return state
+}
+
+export default posts
