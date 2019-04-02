ab32738fe1ec63b03746421284cd3def4f2101e6
Ross Hathaway <hathaway.ross@gmail.com>
2019-04-02 00:17:33 -0400

    have action creators, reducers,and map dispatch and state to props for main component

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {render} from 'react-dom'
 import css from './styles/style.styl'
-import Main from './components/Main.js'
+import App from './components/app.js'
 import Photogrid from './components/Photogrid';
 import Single from './components/Single';
 import {Router, Route, IndexRoute, browserHistory} from 'react-router'
@@ -9,12 +9,14 @@ import {Provider} from 'react-redux'
 import store, {history} from './store'
 
 const router = (
-  <Router history={browserHistory}>
-    <Route path="/" component={Main}>
-      <IndexRoute component={Photogrid} />
-      <Route path="/view/:postId" component={Single}/>
-    </Route>
-  </Router>
+  <Provider store={store}>
+    <Router history={history}>
+      <Route path="/" component={App}>
+        <IndexRoute component={Photogrid} />
+        <Route path="/view/:postId" component={Single}/>
+      </Route>
+    </Router>
+  </Provider>
 )
 
 render(router, document.getElementById('root'))
